501998e1e139a84644c0dda0bf79574c5c2696f7
PatRider64 <83703900+PatRider64@users.noreply.github.com>
2024-04-11 09:37:40 +0200

    Exercice 4

--- src/fr/patrik/OperationAdd.java
+++ src/fr/patrik/OperationAdd.java
@@ -1,8 +1,11 @@
 package fr.patrik;
 
+import java.util.function.BiFunction;
+
 public class OperationAdd implements Operation {
    @Override
    public int doOperation(int num1, int num2) {
-      return num1 + num2;
+      BiFunction<Integer, Integer, Integer> add = (i, j) -> i+j;
+      return add.apply(num1, num2);
    }
 }
--- src/fr/patrik/OperationMultiply.java
+++ src/fr/patrik/OperationMultiply.java
@@ -1,8 +1,11 @@
 package fr.patrik;
 
+import java.util.function.BiFunction;
+
 public class OperationMultiply implements Operation {
    @Override
    public int doOperation(int num1, int num2) {
-      return num1 * num2;
+      BiFunction<Integer, Integer, Integer> multiply = (i, j) -> i*j;
+      return multiply.apply(num1, num2);
    }
 }
